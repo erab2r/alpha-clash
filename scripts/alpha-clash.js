bb9c59667e49df4954f8feecdf274ba58938b128
erab2r <shkera213@gmail.com>
2024-10-27 13:20:25 +0600

    repo

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,18 @@
 
+// function play(){
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     //show the playground
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+
+// }
+function continueGame(){
+    const alphabet = getRandomAlphabet();
+}
+
 function play(){
-    console.log('play start now');
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
 }
